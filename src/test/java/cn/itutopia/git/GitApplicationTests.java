a60e69823d8afca2abe77ef7586617b18f988010
junchao.li <junchao.li@tongfudun.com>
2022-04-13 07:10:29 +0800

    init

---
+++ src/test/java/cn/itutopia/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itutopia.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
